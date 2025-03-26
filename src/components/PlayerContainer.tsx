0d6df545332ba501edef9a2a5da44617315f72da
mdellabani <mdellabani@gmail.com>
2025-03-26 17:26:26 +0100

    add

---
+++ src/components/PlayerContainer.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { usePlayer } from '@/context/PlayerContext'
+import AudioPlayer from './AudioPlayer'
+
+const PlayerContainer = () => {
+  const { playlist, showPlayer } = usePlayer()
+
+  if (!playlist.length || !showPlayer) {
+    return null
+  }
+
+  return (
+    <div className="flex h-screen flex-col">
+      <div className="flex-grow"></div>
+      <div className="mt-auto">
+        <AudioPlayer />
+      </div>
+    </div>
+  )
+}
+
+export default PlayerContainer
