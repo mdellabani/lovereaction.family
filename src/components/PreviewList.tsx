3708f7a010d12eae58b47c34ad619f99f4957c8b
mdellabani <mdellabani@gmail.com>
2025-03-31 18:57:46 +0200

    see more link

--- src/components/PreviewList.tsx
+++ src/components/PreviewList.tsx
@@ -20,39 +20,44 @@ const PreviewList = <T extends PreviewItem>({
   const resolvedItems = items()
 
   return (
-    <div className="relative m-10 w-2/3 border-b-4 border-l-4 border-gray-300 p-4 before:absolute before:left-0 before:top-0 before:w-1/4 before:border-t-4 before:border-gray-300">
-      <h2 className="mb-4 pl-2 text-xl font-bold">{title}</h2>
-      {loading ? (
-        <div className="flex flex-row items-center justify-center">
-          <Spinner color="danger" labelColor="danger" />
-        </div>
-      ) : (
-        <div className="flex gap-4">
-          {resolvedItems.slice(0, 4).map((item, index) => (
-            <div
-              className="w-48 rounded-lg border p-4 text-center shadow-md"
-              key={index}
-            >
-              <Image
-                alt={item.title}
-                className="h-40 w-40 rounded-lg object-cover"
-                height={200}
-                src={item.imageUrl}
-                width={200}
-              />
-              <p className="mt-2 text-sm text-gray-500">[{item.type}]</p>
-              <p className="font-bold">{item.title}</p>
-              {item.artist && (
-                <p className="text-sm text-gray-600">{item.artist}</p>
-              )}
-            </div>
-          ))}
-        </div>
-      )}
+    <div className="relative m-10 flex  w-2/3 items-center gap-4 border-b-4 border-l-4 border-gray-300 p-4 before:absolute before:left-0 before:top-0 before:w-1/4 before:border-t-4 before:border-gray-300">
+      <div>
+        <h2 className="mb-4 pl-2 text-xl font-bold">{title}</h2>
+        {loading ? (
+          <div className="flex flex-row items-center justify-center">
+            <Spinner color="danger" labelColor="danger" />
+          </div>
+        ) : (
+          <div className="flex gap-4">
+            {resolvedItems.slice(0, 4).map((item, index) => (
+              <div
+                className="w-48 rounded-lg border p-4 text-center shadow-md"
+                key={index}
+              >
+                <Image
+                  alt={item.title}
+                  className="h-40 w-40 rounded-lg object-cover"
+                  height={200}
+                  src={item.imageUrl}
+                  width={200}
+                />
+                <p className="mt-2 text-sm text-gray-500">[{item.type}]</p>
+                <p className="font-bold">{item.title}</p>
+                {item.artist && (
+                  <p className="text-sm text-gray-600">{item.artist}</p>
+                )}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
       {resolvedItems.length > 2 && (
         <div className="mt-4 text-center">
-          <Link className="text-blue-500 hover:underline" href={route}>
-            See More
+          <Link
+            className="ml-auto flex h-10 w-10 items-center justify-center rounded-full border border-gray-400 text-xl text-gray-600 transition hover:bg-gray-600 hover:text-white"
+            href={route}
+          >
+            ⋯
           </Link>
         </div>
       )}
