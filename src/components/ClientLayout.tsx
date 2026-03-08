5a27d1f8b1b080bc48110c972c2ad0319bd0dbf2
mde <mde@ext.activeviam.com>
2026-03-08 17:13:31 +0100

    add missing files

---
+++ src/components/ClientLayout.tsx
@@ -0,0 +1,44 @@
+'use client'
+import Footer from '@/components/Footer'
+import PlayerContainer from '@/components/PlayerContainer'
+import ThemedHeader from '@/components/ThemedHeader'
+import { PlayerProvider } from '@/context/PlayerContext'
+import { HeroUIProvider } from '@heroui/react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { useState } from 'react'
+import PasswordProtection from '@/components/PasswordProtection'
+
+export default function ClientLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  return (
+    <HeroUIProvider>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="system"
+        disableTransitionOnChange
+        enableSystem
+      >
+        <PlayerProvider>
+          <PasswordProtection setAuthenticated={setIsAuthenticated} />
+          {isAuthenticated && (
+            <>
+              <ThemedHeader />
+              <div className="mx-auto min-h-screen max-w-3xl px-4 pt-16 sm:px-8 md:px-16">
+                <div className="flex h-full flex-col gap-10 md:gap-20">
+                  {children}
+                  <PlayerContainer />
+                  <Footer />
+                </div>
+              </div>
+            </>
+          )}
+        </PlayerProvider>
+      </NextThemesProvider>
+    </HeroUIProvider>
+  )
+}
